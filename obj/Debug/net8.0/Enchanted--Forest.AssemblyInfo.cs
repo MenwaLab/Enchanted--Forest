0497600c40b6f03508643b16d7b58b14ba686746
MenwaLab <meylijv@gmail.com>
2025-01-01 14:58:20 -0500

    sigue sin cambios, pero problema con commits. sigue arreglado ciclo infinito de mov invalidos

--- obj/Debug/net8.0/Enchanted--Forest.AssemblyInfo.cs
+++ obj/Debug/net8.0/Enchanted--Forest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Enchanted--Forest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef6061e2b1f7a63c3d621d9f1e2158a3021eaec2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0600054b0f9f0a42837170d179328398db82286")]
 [assembly: System.Reflection.AssemblyProductAttribute("Enchanted--Forest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Enchanted--Forest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
